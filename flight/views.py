3db929bfdc5d4e84bf42e0f5df4b1cf6eaddcce0
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-29 21:00:41 +0300

    .

--- flight/views.py
+++ flight/views.py
@@ -3,6 +3,8 @@ from .serializers import FlightSerializer
 from rest_framework import viewsets
 from .models import Flight, Passenger, Reservation
 
+#! Thanks to the ModelViewSet, we can do all the operations 👇
+#!  GET, POST, PUT, DELETE, PATCH
 class FlightView(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
